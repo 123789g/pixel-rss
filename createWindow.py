1889d6746b4c08dd36cac683de8b4d0fbf348dd0
Galen <luther_the_developer@proton.me>
2024-03-14 10:19:10 -0400

    Styles working. Dragging doesn't. Still need resizing functions re-implemented.

--- createWindow.py
+++ createWindow.py
@@ -1,6 +1,23 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+# This class recreates functionality for our window. In particular, the ability to click and drag the window.
+class customTitleBar(Frame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.master = master
+        self.bind("<ButtonPress-1>", self.start_drag)
+        self.bind("<B1-Motion>", self.on_drag)
+
+    def start_drag(self, event):
+        self._drag_start_x = event._drag_start_x
+        self._drag_start_y = event._drag_start_y
+
+    def on_drag(self, event):
+        x = self.master.winfo_x() + event.x - self._drag_start_x
+        y = self.master.winfo_y() + event.y - self._drag_start_y
+        self.master.geometry(f"+{x}+{y}")
+
 root = Tk()
 root.overrideredirect(True)
 root.configure(background="#363636")
@@ -20,5 +37,9 @@ titlebar.pack(side="top", fill="x")
 
 closeButton = Button(titlebar, image=closeImg, command=root.destroy, borderwidth=0, bg="#363636")
 closeButton.pack(side="right")
+maximizeButton = Button(titlebar, image=maximizeImg, borderwidth=0, bg="#363636")
+maximizeButton.pack(side="right")
+minimizeButton = Button(titlebar, image=minimizeImg, borderwidth=0, bg="#363636")
+minimizeButton.pack(side="right")
 
 root.mainloop()
