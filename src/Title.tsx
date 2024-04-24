f8ec25b547c8100909f3ff65cf8085fb8797c472
Galen <luther_the_developer@proton.me>
2024-04-24 08:18:54 -0400

    Icons Added for Title Bar. Functions are a bit "sticky"? Sometimes it takes multiple clicks for Maximize to toggle.

--- src/Title.tsx
+++ src/Title.tsx
@@ -1,8 +1,12 @@
 import "./Title.css";
 import { appWindow } from "@tauri-apps/api/window";
+import Close from "./assets/titlebar/Close.png"
+import Maximize from "./assets/titlebar/Maximize.png"
+import Minimize from "./assets/titlebar/Minimize.png"
 
 function Title() {
 
+  // Window Functions abstracted to their own functions because calling them inline onClick made them unresponsive.
   const minimizeWindow = () => {
     appWindow.minimize()
   }
@@ -19,20 +23,20 @@ function Title() {
         <div data-tauri-drag-region className="titlebar">
           <div className="titlebar-button" id="titlebar-minimize">
             <img
-              src="https://api.iconify.design/mdi:window-minimize.svg"
+              src={Minimize}
               alt="minimize"
               onClick={minimizeWindow}
             />
           </div>
           <div className="titlebar-button" id="titlebar-maximize">
             <img
-              src="https://api.iconify.design/mdi:window-maximize.svg"
+              src={Maximize}
               alt="maximize"
               onClick={maximizeWindow}
             />
           </div>
           <div className="titlebar-button" id="titlebar-close">
-            <img src="https://api.iconify.design/mdi:close.svg" alt="close" onClick={closeWindow} />
+            <img src={Close} alt="close" onClick={closeWindow} />
           </div>
         </div>
       </body>
